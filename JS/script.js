76076dceec0a53712cb2f07fbb3cd4e972b5182f
Tiagorcp <tiagorcp2609@gmail.com>
2022-11-13 14:58:52 -0300

    Finalizando Projeto Mario Jump
    Projeto Jogo Mario Jump

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
-const   jump = () => {
+const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
@@ -9,14 +9,26 @@ const   jump = () => {
     }, 500);
 }
 
-const loop = setInterval(() =>{
+const loop = setInterval(() => {
+
+    console.log('loop')    
 
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom;
-    if (pipePosition <= 120) {
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
+    if (pipePosition <= 120 && pipePosition> 0 && marioPosition < 80) {
+        
         pipe.style.animation = 'none';
-        pipe.style.left = '${pipePosition}px';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './Imagens/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
 
     }
 
